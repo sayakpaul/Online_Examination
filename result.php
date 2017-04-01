826e4cfb4ba9bb928b8669395fec5afc6b674fc2
Sayak Paul <spsayakpaul@gmail.com>
2017-04-01 12:42:03 +0530

    Update result.php

--- result.php
+++ result.php
@@ -1,7 +1,6 @@
 <?php
 	include "connect.php";
 ?>
-
 <?php
 	$totq = $_REQUEST['totq'];
 	//echo $totq;
@@ -10,15 +9,19 @@
 	{
 		$qid = $_REQUEST['q'.$i];
 		$ans = $_REQUEST['r'.$i];
-		$q = "select cans from question where qid = $qid";
+		$q = "select * from question where qid = $qid";
 		$s = mysql_query($q);
 		if($r = mysql_fetch_array($s))
 		{
-			$cans = $r[0];
+			$question = $r[2];
+			$cans = $r[7];
+			echo "<h4>".$i.". ".$question." - ".$cans;
+			echo "<br>";
 			if($cans == $ans)
 				$m++;
 		}
+		
 	}
-	echo "<h1> Your Score is ".$m." out of ".($totq - 1);
+	echo "<h1><center> Your Score is ".$m." out of ".($totq - 1);
 	mysql_close();
 	?>
